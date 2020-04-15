97324faae0cf33d6f0e8950f5a0743ffe50f0651
Diki <dikibhuyan@gmail.com>
2020-04-15 16:11:23 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let name;
+	import '../node_modules/materialize-css/dist/css/materialize.min.css'
+	import '../node_modules/materialize-css/dist/js/materialize.min.js'
+	import { Router, Route } from "svelte-routing";
+	import  Navbar  from './layout/Navbar.svelte';
+	import  Home  from './pages/Home.svelte';
+	import  Class  from './pages/Class.svelte';
+</script>
+
+<style>
+
+</style>
+
+<Router>
+	<Navbar />
+	<div class="container">
+		<Route path="/" component={Home} />
+		<Route path="/class" component={Class} />
+	</div>
+</Router>
---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,13 @@
+  <script>
+    import { Link } from 'svelte-routing';
+  </script>
+  
+  <nav>
+    <div class="nav-wrapper">
+      <Link to="/" class="brand-logo">ClassRoom</Link>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/class'>Class</Link></li>
+      </ul>
+    </div>
+  </nav>
